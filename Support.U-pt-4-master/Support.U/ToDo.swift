488ae41006c20d9eaded43b6fc3fd52dcc840930
laura-pena <69254524+laura-pena@users.noreply.github.com>
2020-08-12 15:17:15 -0400

    Add files via upload
    Iteration 2.1

---
+++ Support.U-pt-4-master/Support.U/ToDo.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo.swift
+//  Support.U
+//
+//  Created by Laura Pena on 8/12/20.
+//  Copyright © 2020 Sonal Bhatia. All rights reserved.
+//
+
+import UIKit
+class ToDoClass {
+    var name = ""
+    var email = ""
+    var school = ""
+    var grade = ""
+    var myClass = ""
+    
+}
